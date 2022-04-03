e1b656681b0af6d38742d961563407c8fa6525eb
CrzTosheba <fugu@list.ru>
2022-04-03 16:27:11 +0300

    Добавьте файлы проекта.

---
+++ FinalTask9.6/Program.cs
@@ -0,0 +1,84 @@
+﻿public class Program
+{
+    static int Division(int a, int b)
+    {
+        return a / b;
+    }
+  
+
+    static void Main(string[] args)
+    {
+        Exception exception = new Exception();
+        
+        try
+        {
+            int result = Division(7, 0);
+
+            Console.WriteLine(result);
+        }
+
+        catch (System.DivideByZeroException)
+        {
+            Console.WriteLine("На ноль делить нельзя!");
+        }
+        try
+        {
+            int result = Division(7, 0);
+
+            Console.WriteLine(result);
+        }
+
+        catch (Exception)
+        {
+            Console.WriteLine("Сработало мое исключение");
+        }
+
+
+        try
+        {
+            FutureFeature();
+        }
+        catch (NotImplementedException) //Это исключение выбрасывается, когда запрошенный метод или операция не реализованы.
+        {
+            Console.WriteLine("Метод не реализован");
+        }
+        try
+        {
+
+            string dir = @"c:\XYZ";// пытаемя обратиться к директории
+
+            Directory.SetCurrentDirectory(dir);
+        }
+        catch (DirectoryNotFoundException dirEx) // путь не найден, ну или директория
+        {
+          Console.WriteLine("А ни чего и нету: " + dirEx.Message);
+        }
+        int value = 780000000;
+        checked
+        {
+            try
+            {
+               
+                int square = value * value;
+                Console.WriteLine("{0} ^ 2 = {1}", value, square);
+            }
+            catch (OverflowException) // выход за диапозон значений, в данном случае выход за int тоесть 2 147 483 647, при умножении должно быть 608 400 000 000 000 000, но мы не сможем вывести это число.
+            {
+                double square = Math.Pow(value, 2);
+                Console.WriteLine("Exception: {0} > {1:E}.",
+                                  square, Int32.MaxValue);
+            }
+        }
+
+    }
+    static void FutureFeature()
+    {
+       
+        throw new NotImplementedException();
+    }
+
+
+
+    
+    
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+
+public class ReverseComparer : IComparer
+{
+    // Вызов CaseInsensitiveComparer.Compare с обратными параметрами.
+    public int Compare(Object x, Object y)
+    {
+        return (new CaseInsensitiveComparer()).Compare(y, x);
+    }
+}
+
+public class Example
+{
+
+    public static void Main()
+    {
+        Exception exception = new Exception("Собственное исключения");
+        Console.WriteLine("Введите 5 фамилий");
+
+        String[] words = new string[5];
+
+       
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = TryGetValue(string.Format("Введите фамилию №{0}", i + 1));
+
+        }
+        
+      
+
+
+        // Создаем экземпляр обратного компаратора.
+        IComparer revComparer = new ReverseComparer();
+
+        // Отображаем значения массива
+        Console.WriteLine("Вы ввели:");
+        DisplayValues(words);
+
+
+
+        try
+        {
+            Console.WriteLine("Введите тип сортировки 1 или 2");
+            string? str = Console.ReadLine();
+            if (str != "1" || str != "2")
+            {
+                throw new Exception("Что-то вы не то выбрали");
+            }
+            else
+            {
+                if (str == "1")
+                {
+                    // Сортируем весь массив, используя компаратор по умолчанию
+                    Array.Sort(words);
+                    Console.WriteLine("Сортировка по умолчанию:");
+                    DisplayValues(words);
+                }
+                else if (str == "2")
+
+                {
+                    // Сортируем весь массив с помощью обратного компаратора
+                    Array.Sort(words, revComparer);
+                    Console.WriteLine("Сортировка в обратном порядке:");
+                    DisplayValues(words);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка: {e.Message}");
+        }
+        Console.ReadLine();
+
+    }
+
+    public static void DisplayValues(String[] arr)
+    {
+        for (int i = arr.GetLowerBound(0); i <= arr.GetUpperBound(0);
+              i++)
+        {
+            Console.WriteLine("   [{0}] : {1}", i, arr[i]);
+        }
+        Console.WriteLine();
+    }
+    static string TryGetValue(string message) // метод на проверку пустого значения, нам же не нужны пустые значения
+    {
+        while (true)
+        {
+            Console.WriteLine(message);
+            var result = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(result))
+            {
+                Console.WriteLine("Введено не корректное значение");
+                continue;
+            }
+
+            return result;
+        }
+    }
+}
